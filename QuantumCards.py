287c5add657724928f28a23ae2727ebf9ad3f51b
Samuli Jaaskelainen <jaaskelainen.samuli@gmail.com>
2019-02-16 17:40:53 +0200

    Add button hover. Add player number selection. Add starting player randomized.

--- QuantumCards.py
+++ QuantumCards.py
@@ -1,17 +1,22 @@
 import pygame as pg
+import random
 
 pg.init()
 pg.display.set_caption('Quantum Cards')
 pg.display.set_icon(pg.image.load('Icon.png'))
 screen = pg.display.set_mode((1280, 720))
-COLOR_INACTIVE = pg.Color('lightskyblue3')
-COLOR_ACTIVE = pg.Color('dodgerblue2')
+COLOR_INACTIVE = pg.Color('gray')
+COLOR_ACTIVE = pg.Color('pink')
 COLOR_BLACK = pg.Color('black')
 FONT = pg.font.Font(None, 32)
 BG = pg.image.load("Bg.png")
 BG_RECT = BG.get_rect()
 BUTTON = pg.image.load("Button.png")
+BUTTON_HOVER = pg.image.load("ButtonHover.png")
 BUTTON_PRESSED = pg.image.load("ButtonPressed.png")
+number_of_players=3
+buttons_players=[]
+frames=0
 
 class InputBox:
 
@@ -53,35 +58,104 @@ class InputBox:
 
 class ButtonBox:
 
-    def __init__(self, x, y, w, h, img_not_pressed, img_pressed):
+    def __init__(self, x, y, w, h, img_not_pressed, img_hover, img_pressed, toggle, button_event):
         self.rect = pg.Rect(x, y, w, h);
         self.not_pressed = img_not_pressed
+        self.img_hover = img_hover
         self.img_pressed = img_pressed
+        self.button_event = button_event;
+        self.toggle = toggle
+        self.pressed = False
+        self.lastPressTime = 0
+
+    def press_event(self):
+        self.button_event()
+        self.lastPressTime = frames
+        print('ticks', self.lastPressTime)
+        if(self.toggle):
+            self.pressed = not self.pressed;
 
     def handle_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
-                print('Button pressed')
+                self.button_event()
+                if(self.toggle):
+                    self.pressed = not self.pressed;
+
+    def reset(self):
+        self.pressed = False;
 
     def draw(self, screen):
         if self.rect.collidepoint(pg.mouse.get_pos()):
+            if pg.mouse.get_pressed()[0]:
+                screen.blit(self.img_pressed, self.rect)
+            else:
+                screen.blit(self.img_hover, self.rect)
+        elif self.pressed:
             screen.blit(self.img_pressed, self.rect)
         else:
             screen.blit(self.not_pressed, self.rect)
 
+def SetPlayerToThree():
+    global buttons_players
+    print('Set player number to three')
+    print('L', len(buttons_players))
+    for button in buttons_players:
+        button.reset()
+    number_of_players=3;
+    GetStartingPlayer()
+    
+def SetPlayerToFour():
+    global buttons_players
+    print('Set player number to four')
+    for button in buttons_players:
+        button.reset()
+    number_of_players=4;
+    GetStartingPlayer()
+    
+def SetPlayerToFive():
+    global buttons_players
+    print('Set player number to five')
+    for button in buttons_players:
+        button.reset()
+    number_of_players=5;
+    GetStartingPlayer()
+    
+def GetStartingPlayer():
+    starting_player = random.randint(0, number_of_players) + 1
+
+def Simulate():
+    print('simulate result')
+
+def SimulateWithNoise():
+    print('simulate result with noise')
+
+def Calculate():
+    print('calculate result with quantum computer')
+    
 def main():
     clock = pg.time.Clock()
+    frames = 0
     
     game1 = InputBox(150, 500, 1000, 32)
     game2 = InputBox(150, 540, 1000, 32)
-    input_boxes = [game1, game2]
+    game3 = InputBox(150, 580, 1000, 32)
+    input_boxes = [game1, game2, game3]
     
-    button1 = ButtonBox(30,500,100,32, BUTTON, BUTTON_PRESSED)
-    button2 = ButtonBox(30,540,100,32, BUTTON, BUTTON_PRESSED)
-    buttons = [button1,button2]
+    button_three_p = ButtonBox(30,20,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, True, SetPlayerToThree)
+    button_four_p = ButtonBox(160,20,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, True, SetPlayerToFour)
+    button_five_p = ButtonBox(290,20,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, True, SetPlayerToFive)
+    button_random_p = ButtonBox(500,20,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, False, GetStartingPlayer)
+    button_simulate = ButtonBox(200,680,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, False, Simulate)
+    button_simulate_noisy = ButtonBox(350,680,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, False, SimulateWithNoise)
+    button_calculate = ButtonBox(500,680,100,32, BUTTON, BUTTON_HOVER, BUTTON_PRESSED, False, Calculate)
+    buttons_players.append(button_three_p)
+    buttons_players.append(button_four_p)
+    buttons_players.append(button_five_p)
+    buttons = [button_random_p, button_simulate, button_simulate_noisy, button_calculate]
+    button_five_p.press_event()
     
     running = True
-
     while running:
 
         for event in pg.event.get():
@@ -90,6 +164,9 @@ def main():
 
             for box in input_boxes:
                 box.handle_event(event)
+                
+            for button in buttons_players:
+                button.handle_event(event)
                 
             for button in buttons:
                 button.handle_event(event)
@@ -102,12 +179,28 @@ def main():
 
         for box in input_boxes:
             box.draw(screen)
-            
-        for button in buttons:
+
+        player_buttons_pressed = 0
+        for button in buttons_players:
+            if button.pressed:
+                player_buttons_pressed += 1
+        
+        for button in buttons_players:
             button.draw(screen)
 
+        for button in buttons:
+            button.draw(screen)
+            
+        #if player_buttons_pressed is not 1:
+        #    ticks = 9999999
+        #    for button in buttons_players:
+        #        if button.lastPressTime < ticks:
+        #            ticks = button.lastPressTime
+        #            button.reset()
+
+        frames += 1
         pg.display.flip()
-        clock.tick(30)
+        clock.tick(60)
 
 if __name__ == '__main__':
     main()
